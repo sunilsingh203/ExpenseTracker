977ede97971ee20edf4ddc71ac8501e5eadca2ef
sunilsingh203 <sunilsingh81188@gmail.com>
2025-04-05 19:42:05 +0530

    added the repository and service layer

---
+++ app/src/main/java/org/example/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package org.example.repository;
+
+
+import org.example.entities.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
+}
---
+++ app/src/main/java/org/example/service/UserDetailsServiceImpl.java
@@ -0,0 +1,50 @@
+package org.example.service;
+
+import org.example.entities.UserInfo;
+
+import org.example.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+@Data
+public class UserDetailsServiceImpl implements UserDetailsService
+{
+
+    @Autowired
+    private final UserRepository userRepository;
+
+
+
+    private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+
+        log.debug("Entering in loadUserByUsername Method...");
+        UserInfo user = userRepository.findByUsername(username);
+        if(user == null){
+            log.error("Username not found: " + username);
+            throw new UsernameNotFoundException("could not found user..!!");
+        }
+        log.info("User Authenticated Successfully..!!!");
+        return new CustomUserDetails(user);
+    }
+
+
+}
